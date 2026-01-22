389d8778ebbe8fcb4f0a45d0bd228fa52bc68596
박지섭 <qkrwltjq2426@naver.com>
2026-01-22 13:27:14 +0900

    Design: tailwind css theme 정의

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <>
-      <h1>final-project-sample v05</h1>;
+      <h1 className="text-red-500 text-4xl">final-project-sample v05</h1>
+      <h1 className="text-br-error">final-project-sample v05</h1>
       <h2>client-id: {process.env.NEXT_PUBLIC_CLIENT_ID}</h2>
-      <div className="bg-primary text-secondary font-pretendard">
+      <div className="bg-br-primary-500 text-br-button-disabled-text font-pretendard">
         커스텀 스타일
       </div>
     </>
