a1caf1b9fe87b21709aea6eaecbd51687b0fbefb
esjsjsj <esj.esj.sj@gmail.com>
2026-02-05 16:11:29 +0900

    Test: 판매 목록 기능 구현중 임시 코드

--- app/mypage/sales/page.tsx
+++ app/mypage/sales/page.tsx
@@ -3,11 +3,47 @@
 import UnderBar from '@/components/common/Footer';
 import Header from '@/components/common/Header';
 import ProductList from '@/components/search/ProductList';
-import { useState } from 'react';
+import { getSellerProductList } from '@/lib/api/products';
+import { getUserInfo } from '@/lib/api/users';
+import { SellerProductList } from '@/types/product';
+import { useEffect, useState } from 'react';
 
 //판매내역
 export default function SalesPage() {
   const [activeTab, setActiveTab] = useState('selling');
+  const [products, setProducts] = useState<SellerProductList[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSellerProducts = async () => {
+      setIsLoading(true);
+
+      // 현재 로그인한 유저 정보 가져오기
+      const { ok: userOk, item: user } = await getUserInfo();
+
+      if (userOk === 1 && user) {
+        // 판매자 상품 목록 가져오기
+        const response = await getSellerProductList(user._id);
+
+        // ok === 1일 때만 item이 존재
+        if (response.ok === 1) {
+          setProducts(response.item);
+        }
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchSellerProducts();
+  }, []);
+  // 판매 중 / 판매 완료 필터링 --- 작업중 ----
+  // extra?.isSoldOut 또는 quantity로 구분하기????  타입수정필요할듯?
+  // const sellingProducts = products.filter(
+  //   p => !p._id.isSoldOut && p.quantity > 1
+  // );
+  // const soldProducts = products.filter(
+  //   p => p.extra?.isSoldOut || p.quantity === 1
+  // );
 
   return (
     <>
@@ -41,20 +77,10 @@ export default function SalesPage() {
       </div>
       <div>
         {/* 상품 목록 */}
-        {/* 상품 목록 */}
         <main className="flex-1 px-4">
           {/* 판매 중 탭 */}
           {activeTab === 'selling' && (
             <>
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
-              <ProductList />
               <ProductList />
             </>
           )}
@@ -62,7 +88,6 @@ export default function SalesPage() {
           {/* 판매 완료 탭 */}
           {activeTab === 'sold' && (
             <>
-              <ProductList />
               <ProductList />
             </>
           )}
