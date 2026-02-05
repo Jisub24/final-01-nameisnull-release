da483b87b05255c8d4bc2b8631d4bc917735adcb
박지섭 <qkrwltjq2426@naver.com>
2026-02-05 18:40:26 +0900

    Modify: lib/api/user.ts - getUserInfo 함수 수정

--- lib/api/users.ts
+++ lib/api/users.ts
@@ -1,24 +1,26 @@
+import useUserStore from '@/store/authStore';
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
 
 // 마이페이지
 export async function getUserInfo() {
   try {
-    const token = localStorage.getItem('accessToken');
-    const userId = localStorage.getItem('userId');
+    const { accessToken, user } = useUserStore.getState();
 
-    if (!userId) {
+    if (!accessToken || !user) {
       return {
         ok: 0,
         message: '로그인이 필요합니다.',
       };
     }
+    const userId = user._id;
 
     const res = await fetch(`${API_URL}/users/${userId}`, {
       headers: {
         'Client-Id': CLIENT_ID,
         'Content-Type': 'application/json',
-        ...(token && { Authorization: `Bearer ${token}` }),
+        Authorization: `Bearer ${accessToken}`,
       },
     });
 
